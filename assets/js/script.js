19ac257f6f8541f3e381b97e6cf6752b72d5c62d
Jason L <jkleitz27@gmail.com>
2022-04-21 18:51:49 -0400

    start of criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+    var criteria= getCriteria();
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
@@ -15,3 +13,37 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Code to run 
+function getCriteria() {
+    var userCriteria = {
+        length: 8,
+        includelowerCase: true,
+        includeupperCase: false,
+        includenumbers: false,
+        includespecialCharacters: false,
+
+    }
+}
+
+// functinons for characters
+
+function getLowerCaseCharacters(){
+    var lowerCase = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    return lowerCase
+}
+
+function getUpperCaseCharacters (){
+    var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    return upperCase
+}
+
+function getNumberList (){
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    return numbers
+}
+
+function getSpecialCharacters(){
+    var specialCharacters =["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~",'"'];
+    return specialCharacters 
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Password Generator
+
+## Purpose
+To create passwords of between 8 and 128 characters that include different criteria if user chooses to use that criteria 
+
+## Criteria
+
+User is asked whether or not to include lowercase, uppercase, numeric, and/or special characters with different prompts for each item
+
+## Built with 
+* HTML
+* CSS
+* JavaScript
+
+## Website
+
+##
+<p align="center">
+  <img src="./assets/images/portfolio-site.png" width="350"
+
+</p>
+
+## Contribution
+Made by Jason Leitz
